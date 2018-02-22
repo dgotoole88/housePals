5b4e37a7e779c1fd8a5838c6a0fbb97c28c545fb
dgotoole88 <cell321@hotmail.com>
2018-02-22 12:29:54 +1000

    update 22/02/2018

--- mvc/index.php
+++ mvc/index.php
@@ -11,11 +11,20 @@
         <div id="container">
             <div id="topRow">
                 <div class="smallRow">
+                    <div class="alignText">
+                        <h3 class="divTitle">Next Bill Due</h3>
+                        <h4 class="divText">Power Bill $350</h4>
+                        <h4 class="divText">Date: 20/02/2018</h4>
+                    </div>
                     <div class="enterDiv">
                         <h2 class="enterDivText">Bills</h2>
                     </div>
                 </div>
                 <div class="smallRow">
+                    <div class="alignText">
+                        <h3 class="divTitle">You owe a total of</h3>
+                        <h4 class="divText">$100</h4>
+                    </div>
                     <div class="enterDiv">
                         <h2 class="enterDivText">IOU</h2>
                     </div>
